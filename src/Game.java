7c60ea1f87c88170159296c86b474cb1f5894724
DaeyongK <>
2022-03-31 13:45:58 -0400

    Got transparent background to work for base model character

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 public class Game {
@@ -16,7 +20,15 @@ public class Game {
 			return false;
 		}
 	}
-	public Game() {
+	private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+		Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
+		BufferedImage newImage = new BufferedImage(resultingImage.getWidth(null), resultingImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = newImage.createGraphics();
+		g.drawImage(resultingImage, 0, 0, null);
+		g.dispose();
+		return newImage;
+	}
+	public Game() throws IOException {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame = new JFrame("Attack on Anime");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,9 +37,17 @@ public class Game {
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		contentPane.setBackground(new Color(255,255,255));
+		BufferedImage p1 = resizeImage(ImageIO.read(new File("images/BaseCharacterImagepng.png")), 300,400);
+		JLabel pl1 = new JLabel(new ImageIcon(p1));
+		pl1.setBounds(0, 0, 300, 400);
+		contentPane.add(pl1);
 		frame.setContentPane(contentPane);
 		frame.pack();
 		frame.setVisible(true);
-		frame.setSize(1400, 850);
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) size.getWidth();
+		int height = (int) size.getHeight();
+		frame.repaint();
+		frame.setSize(width, height);
 	}
 }
