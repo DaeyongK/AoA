f5960db0833614fa9da98de14f9f25bc1753ad87
DaeyongK <>
2022-04-18 14:47:30 -0400

    Removed unnecessary code. Added bound.png

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,8 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
 public class Game {
 	private JFrame frame;
 	private GameScreen contentPane;
-	public GameScreen getGameScreen() {
-		return contentPane;
-	}
 	public Game() throws IOException, InterruptedException {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame = new JFrame("Attack on Anime");
